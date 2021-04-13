eb25a0a8fa4efc44ad0e66a3ffc69b8564f1c0d9
Laís Galvão <31069486+LaisGalvao@users.noreply.github.com>
2021-04-13 15:40:50 -0300

    reiniciando a listagem
    perdi meu trabalho feito anteriormente

--- js/index.js
+++ js/index.js
@@ -1,85 +1,131 @@
 
-//app está sendo carregada nosubmit
-var app = new Vue({
-    el: '#app',
-    
-   
-
-      //minha função que puxa os dados para o  localStorage
-      salvar() {
-        if (localStorage.name) {
-          this.name = localStorage.name;
-        }
-        if (localStorage.email){
-            this.email = localStorage.email;
-        }
-        if (localStorage.pass){
-            this.pass = localStorage.pass;
-        }
-        if (localStorage.repeatPass){
-            this.repeatPass = localStorage.repeatPass;
-        }
-        if (localStorage.cpf){
-            this.cpf = localStorage.cpf;
-        }
-        if (localStorage.tel){
-            this.tel = localStorage.tel;
-        }
-        //aqui eu poderia comparar se as senhas não coincidem
-        /*if(pass != repeatPass){
-            console.log('as senhas não coincidem')
-         }*/
-      },
-     methods: {
-        //função de salvar
-         salvar(){
-             localStorage.name = this.name;
-             localStorage.email = this.email;
-             localStorage.pass = this.pass;
-             localStorage.repeatPass = this.repeatPass;
-             localStorage.cpf = this.cpf;
-             localStorage.tel = this.tel;
-
-         },
-           data(){ 
-                return {
-                    //aqui declaro meus dados que vão para o localStorage
-                    //coloco eles num obj
-                    data: {
-                        name: '',
-                        email: '',
-                        pass: '',
-                        repeatPass: '',
-                        cpf: '',
-                        tel: ''
-                    },
-                    columns: [
-                        {
-                            field: 'first_name',
-                            label: 'First Name',
-                        },
-                        {
-                            field: 'email',
-                            label: 'email',
-                        },
-                        {
-                            field: 'cpf',
-                            label: 'cpf',
-                        
-                        },
-                        {
-                            field: 'telefone',
-                            label: 'telefone',
-                        }
-                    ]
-                }
-            } // aqui acaba a funçao retorna  
-     
-     
-    }
-     
+//navbar
+var nav = new Vue({
+    el:'#nav',
+    template: `
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+            <b-field>
+                <img src="images/analytics.png" alt="Analytics" class="img-head">
+                <b-input placeholder="Buscar funcionalidade.." 
+                class="search"></b-input>
+            </b-field>
+        </nav>`
 })
 
 
+//menu aside
+var menuapp = new Vue({
+    el:'#menu',
+    template: `<aside class="menu">
+        
+    <ul class="menu-list">
+      <li><a><i class="fas fa-home"></i> Dashboard</a></li>
+      <li><a><i class="fas fa-users"></i> Grupos</a></li>
+      <li><a href="users.html"><i class="fas fa-user"></i> Usuários</a></li>
+      <li><a><i class="fas fa-atom"></i> Sistemas</a></li>
+      <li><a><i class="fas fa-sitemap"></i> Categoria</a></li>
+    </ul>
+</aside>`
+})
 
-  
+//container com form de cadastro
+var app = new Vue({
+
+    el:'#app',
+    template: `<div class="container">
+                <section class="hero">
+                    <div class="hero-body">
+                    <p class="title">
+                        Cadastrar novo usuário
+                    </p>
+                    <b-field>  
+                        <b-button class="btnsalva" @click="salvar">Salvar</b-button>
+                    </b-field> 
+                    </div>
+                </section>
+                    
+                    <b-field label="Nome" class="name">
+                        <b-input type="text" placeholder="Seu nome" v-model="name"></b-input>
+                    </b-field>
+
+                    <b-field label="Email" class="email">
+                        <b-input type="email" placeholder="email@exemplo.com" maxlength="30" v-model="email">
+                        </b-input>
+                    </b-field>
+            
+                    <b-field label="Password" class="pass">
+                        <b-input type="password" placeholder="sua senha" maxlength="30" v-model="pass"></b-input>
+                    </b-field>
+                    
+                    <b-field label="Password" class="pass">
+                        <b-input type="password" placeholder="sua senha" maxlength="30" v-model="repeatPass"></b-input>
+                    </b-field>
+
+                    <b-field label="seu CPF" class="cpf">
+                        <b-input placeholder="Seu CPF"
+                            type="number"
+                            min="10"
+                            max="20" v-model="cpf">
+                        </b-input>
+                    </b-field>
+
+                    <b-field label="telefone" class="tel">
+                        <b-input placeholder="(11) 91234-5678"
+                            type="tel"
+                            min="10"
+                            max="12" v-model="tel">
+                        </b-input>
+                    </b-field>
+
+                    <b-field label="Subject">
+                        <b-select placeholder="Selecione um cagro">
+                            <option value="1">Super-Admin</option>
+                            <option value="2">Outro cargo</option>
+                        </b-select>
+                    </b-field>
+            </div>`
+
+
+})
+
+var obj = new Vue({
+    data:{
+        users:[
+            {
+                name: '',
+                email: '',
+                pass: '',
+                repeatPass: '',
+                cpf: '',
+                tel: '',
+                cargo: 2  
+            }
+        ]
+       
+    }
+})
+
+//card que retorna os users cadastrados
+var app2 = new Vue({
+    el: '#app2',
+    template: `<ul>
+                <li v-for="x in users">
+                </li>
+            </ul>`
+})
+
+//botão de cadastrar novo
+var cadnovo = new Vue({
+    el:'#cadnovo',
+    template: `<div class="container">
+            <section class="hero">
+                <div class="hero-body">
+                <p class="title">
+                    Usuários
+                </p>
+                <b-field>  
+                    <b-button class="btnsalva"><a href="submit.html" class="salva">Novo usuário</a></b-button>
+                </b-field> 
+                </div>
+        </section>`
+})
