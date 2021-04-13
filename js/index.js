6b84c591bec4e400dc0c3fce29e62fad63f46067
Laís Galvão <31069486+LaisGalvao@users.noreply.github.com>
2021-04-13 11:30:02 -0300

    inserindo as novas páginas
    alterando um pouco, mudei o nome do index para "submit.html", e adicionando a tela de listagem dos usuários

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-var app;
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: ''
+      },
+      mounted() {
+        if (localStorage.name) {
+          this.name = localStorage.name;
+        }
+        if(localStorage.email){
+            this.email = localStorage.email;
+        }
+        if(localStorage.pass){
+            this.pass = localStorage.pass;
+        }
+        if(localStorage.repeatPass){
+            this.repeatPass = localStorage.repeatPass;
+        }
+        if(pass != repeatPass){
+            console.log('as senhas não coincidem')
+         }
+      },
+     methods: {
+         salvar(){
+             localStorage.name = this.name;
+             localStorage.email = this.email;
+             localStorage.pass = this.pass;
+             localStorage.repeatPass = this.repeatPass;
+            
+         }
+     },
+})
+
+var app2 = new Vue({
+    el: "#app2",
+    
+}) 
