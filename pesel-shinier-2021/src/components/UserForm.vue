290188d4d2e3470d9cd5d40946b70e9febd419a5
Laís Galvão <31069486+LaisGalvao@users.noreply.github.com>
2021-04-15 13:43:16 -0300

    adicionando o novo projeto
    adicionando o novo projeto (agora usando o vue-cli, e com o buefy da forma mais recomendada e correta para este projeto)

--- pesel-shinier-2021/src/components/UserForm.vue
+++ pesel-shinier-2021/src/components/UserForm.vue
@@ -1,77 +1,215 @@
 <template>
   <div class="container">
-                <h1>Cadastrar usuário</h1>
-                <hr>
-                <div class="form-cad form-group">
-                    <p>
-                        <input placeholder="nome" 
-                        class="form-control"
-                        type="text" name="name" 
-                        id="name" v-model="name">
-                    </p>
-                    <p>
-                        <input placeholder="email@example.com" 
-                        class="form-control"
-                        type="email" name="email" 
-                        id="name" v-model="email">
-                    </p>
-                    <p>
-                        <input placeholder="you password.." 
-                        class="form-control"
-                        type="password" name="password" 
-                        id="name" v-model="pass">
-                    </p>
-                    <p>
-                        <input placeholder="123.456.789-00" 
-                        class="form-control"
-                        type="text" name="cpf" 
-                        id="name" v-model="cpf">
-                    </p>
-                    <p>
-                        <input placeholder="(11) 91234-5678" 
-                        class="form-control"
-                        type="tel" name="tel" 
-                        id="name" v-model="tel">
-                    </p>
-                    <button type="submit" 
-                    class="btn btn-primary"
-                    v-on:click="add">
-                        Cadastrar
-                    </button>
-                </div>
+    <section class="hero">
+      <div class="hero-body">
+        <p class="title">Cadastrar novo usuário</p>
+        <b-field>
+          <b-button class="btnsalva" v-on:click="add">Salvar</b-button>
+        </b-field>
+      </div>
+    </section>
+
+    <b-field label="Nome" class="name">
+      <b-input type="text" placeholder="Seu nome" v-model="name"></b-input>
+    </b-field>
+
+    <b-field label="Email" class="email">
+      <b-input
+        type="email"
+        placeholder="email@exemplo.com"
+        maxlength="30"
+        v-model="email"
+      >
+      </b-input>
+    </b-field>
+
+    <b-field label="Password" class="pass">
+      <b-input
+        type="password"
+        placeholder="sua senha"
+        maxlength="30"
+        v-model="pass"
+      ></b-input>
+    </b-field>
+
+    <b-field label="seu CPF" class="cpf">
+      <b-input placeholder="Seu CPF" type="number" min="10" max="20" v-model="cpf">
+      </b-input>
+    </b-field>
+
+    <b-field label="telefone" class="tel">
+      <b-input placeholder="(11) 91234-5678" type="tel" min="10" max="12" v-model="tel">
+      </b-input>
+    </b-field>
+
+    <b-field label="Subject">
+      <b-select placeholder="Selecione um cagro">
+        <option value="1">Super-Admin</option>
+        <option value="2">Outro cargo</option>
+      </b-select>
+    </b-field>
+  </div>
 </template>
+<style>
+.menu {
+  background-color: #050a0c;
+  width: 15%;
+  height: 100vh;
+}
 
+.menu-list {
+  margin-top: 90px;
+}
+
+.img-head {
+  padding: 30px;
+}
+
+.fas {
+  color: #363636;
+}
+
+.navbar {
+  background-color: #050a0c;
+}
+
+.search {
+  width: 80vh;
+  padding: 30px;
+  background-color: #050a0c;
+}
+
+li {
+  margin-top: 20px;
+}
+
+html {
+  background: #050a0c;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #050a0c;
+}
+
+.hero {
+  width: 100%;
+  display: inline-flex;
+  justify-content: center;
+}
+
+.title {
+  color: snow;
+}
+
+.container {
+  width: 80%;
+  height: 120vh;
+  border-radius: 12px;
+  margin-left: 16%;
+  padding-left: 2rem;
+  margin-top: -650px;
+  flex-direction: column;
+  background-color: #363636;
+}
+
+.btnsalva {
+  background-color: #4ecca3;
+  color: snow;
+  float: right;
+  margin-top: -70px;
+  margin-right: 220px;
+}
+
+.salva {
+  text-decoration: none;
+}
+.salva:visited {
+  color: snow;
+}
+
+.media {
+  color: snow;
+  position: relative;
+}
+
+.list-group {
+  width: 60%;
+  height: 10rem;
+  margin-left: 20%;
+  padding-left: 2rem;
+  margin-top: -620px;
+  background-color: #b9b9b9;
+  position: relative;
+}
+
+.name {
+  display: inline-flex;
+  flex-direction: column;
+  width: 40%;
+  color: whitesmoke;
+}
+
+.email {
+  width: 40%;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.pass {
+  margin-top: auto;
+  width: 20%;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.tel {
+  margin-top: auto;
+  width: 20%;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.cpf {
+  margin-top: auto;
+  width: 20%;
+  display: inline-flex;
+  flex-direction: column;
+}
+</style>
 <script>
-  export default {
-    data() {
-      return {
-        name: '',
-        email: '',
-        pass: '',
-        cpf: '',
-        tel: ''
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      pass: "",
+      cpf: "",
+      tel: "",
+    };
+  },
+  methods: {
+    add() {
+      if (this.name.trim() === "" || this.email.trim() === "") {
+        return;
       }
+
+      this.$emit("add-user", {
+        name: this.name,
+        email: this.email,
+        pass: this.pass,
+        cpf: this.cpf,
+        tel: this.tel,
+      });
+
+      this.name = "";
+      this.email = "";
+      this.pass = "";
+      this.cpf = "";
+      this.tel = "";
     },
-    methods: {
-      add() {
-        if(this.name.trim() ==='' || this.email.trim() ===''){
-                        return
-                    }
-
-        this.$emit('add-user', {
-          name: this.name,
-          email: this.email,
-          pass: this.pass,
-          cpf: this.cpf,
-          tel: this.tel
-        });
-
-        this.name = '';
-        this.email = '';
-        this.pass = '';
-        this.cpf = '';
-        this.tel = '';
-      }
-    }
-  }
+  },
+};
 </script>
--- pesel-shinier-2021/src/components/UserList.vue
+++ pesel-shinier-2021/src/components/UserList.vue
@@ -1,53 +1,200 @@
 <template>
-  <div class="container">
-    <h1>Comentários</h1>
-    <hr />
-    <UserForm v-on:add-user="add"></UserForm>
-    <div class="list-group">
-    <p v-if="users.length <= 0">Sem comentários...</p>
-    <div v-else class="list-group-item" v-for="(user, index) in allUsers" v-bind:key="index">
-        <span class="comment__author">Autor: <strong>{{ user.name }}</strong></span>
-        <p>{{ user.message }}</p>
-        <div>
-        <a href="#" title="Excluir" v-on:click.prevent="removeUser(index)">Excluir</a>
-        </div>
+  <div class="list-group">
+    <!--<UserForm v-on:add-user="add"></UserForm>-->
+    <section class="hero">
+      <div class="hero-body">
+        <p class="title">Usuários</p>
+        <b-field>
+          <b-button class="btnsalva"><a href="" class="salva">Novo usuário</a></b-button>
+        </b-field>
+      </div>
+    </section>
+    <p class="nadamostra" v-if="users.length <= 0">Nada a mostrar...</p>
+    <div
+      class="list-group-item"
+      v-else
+      v-for="(user, index) in allUsers"
+      v-bind:key="index"
+    >
+      <span class="user-name">
+        Nome: <strong>{{ user.name }}</strong>
+      </span>
+      <p>{{ user.email }}</p>
+      <p>{{ user.pass }}</p>
+      <p>{{ user.cpf }}</p>
+      <p>{{ user.tel }}</p>
+      <div>
+        <a href="#" title="Excluir" v-on:click.prevent="removeU(index)">Excluir</a>
+      </div>
     </div>
-    </div>
-    <hr />
   </div>
 </template>
+<style>
+.menu {
+  background-color: #050a0c;
+  width: 15%;
+  height: 100vh;
+}
 
+.menu-list {
+  margin-top: 90px;
+}
+
+.img-head {
+  padding: 30px;
+}
+
+.fas {
+  color: #363636;
+}
+
+.navbar {
+  background-color: #050a0c;
+}
+
+.search {
+  width: 80vh;
+  padding: 30px;
+  background-color: #050a0c;
+}
+
+li {
+  margin-top: 20px;
+}
+
+html {
+  background: #050a0c;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #050a0c;
+}
+
+.hero {
+  width: 100%;
+  display: inline-flex;
+  justify-content: center;
+}
+
+.title {
+  color: snow;
+}
+
+.nadamostra {
+  color: snow;
+  margin-top: -30px;
+  position: absolute;
+}
+
+.container {
+  width: 80%;
+  height: 120vh;
+  border-radius: 12px;
+  margin-left: 16%;
+  padding-left: 2rem;
+  margin-top: -650px;
+  flex-direction: column;
+  background-color: #363636;
+}
+
+.btnsalva {
+  background-color: #4ecca3;
+  color: snow;
+  float: right;
+  margin-top: -70px;
+  margin-right: 220px;
+}
+
+.salva {
+  text-decoration: none;
+}
+.salva:visited {
+  color: snow;
+}
+
+.media {
+  color: snow;
+  position: relative;
+}
+
+.list-group {
+  width: 60%;
+  height: 10rem;
+  margin-left: 20%;
+  padding-left: 2rem;
+  margin-top: -620px;
+  background-color: #b9b9b9;
+  position: relative;
+}
+
+.name {
+  display: inline-flex;
+  flex-direction: column;
+  width: 40%;
+  color: whitesmoke;
+}
+
+.email {
+  width: 40%;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.pass {
+  margin-top: auto;
+  width: 20%;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.tel {
+  margin-top: auto;
+  width: 20%;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.cpf {
+  margin-top: auto;
+  width: 20%;
+  display: inline-flex;
+  flex-direction: column;
+}
+</style>
 <script>
-import UserForm from './UserForm';
+import UserForm from "./UserForm";
 export default {
   components: {
-    UserForm
+    UserForm,
   },
   data() {
     return {
-      users: []
-    }
+      users: [],
+    };
   },
   methods: {
-    addUSer(users) {
+    addUser(users) {
       this.users.push(user);
     },
     removeUser(index) {
       this.users.splice(index, 1);
-    }
+    },
   },
   computed: {
     allUsers() {
-      return this.users.map(user => ({
+      return this.users.map((user) => ({
         ...user,
-        name: user.name.trim() === '' ? 'Anônimo' : user.name
-      }))
-    }
+        name: user.name.trim() === "" ? "Anônimo" : user.name,
+      }));
+    },
   },
   watch: {
     comments(val) {
-      console.log('val', val)
-    }
-  }
-}
+      console.log("val", val);
+    },
+  },
+};
 </script>
