88362b1be650a3dc7bddb4acdd94b8b4a1890235
Laís Galvão <31069486+LaisGalvao@users.noreply.github.com>
2021-04-16 19:37:19 -0300

    Add files via upload
    meu arquivo de rotas, meu app e o main.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <b-field>
+        <img src="./assets/analytics.png" alt="Analytics" class="img-head" />
+        <b-input placeholder="Buscar funcionalidade.." class="search"></b-input>
+      </b-field>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -20,3 +26,51 @@ export default {
   component: { UserList, UserForm },
 };
 </script>
+<style>
+.menu {
+  background-color: #050a0c;
+  width: 15%;
+  height: 100vh;
+}
+
+.menu-list {
+  margin-top: 90px;
+}
+
+.img-head {
+  padding: 30px;
+}
+
+.displayed {
+  display: none;
+}
+
+.fas {
+  color: #363636;
+}
+
+.navbar {
+  background-color: #050a0c;
+}
+
+.search {
+  width: 80vh;
+  padding: 30px;
+  background-color: #050a0c;
+}
+
+li {
+  margin-top: 20px;
+}
+
+html {
+  background: #050a0c;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #050a0c;
+}
+</style>
